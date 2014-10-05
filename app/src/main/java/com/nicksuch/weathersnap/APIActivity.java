3534f1c9e7c6ea0c201a9b6bbe514f4029548f79
Nick Such <nicksuch@gmail.com>
2014-10-05 16:50:48 -0400

    New icon, updates to WebActivity

--- app/src/main/java/com/nicksuch/weathersnap/APIActivity.java
+++ app/src/main/java/com/nicksuch/weathersnap/APIActivity.java
@@ -88,7 +88,7 @@ public class APIActivity extends ActionBarActivity implements View.OnClickListen
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_reload) {
             return true;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/nicksuch/weathersnap/WebActivity.java
+++ app/src/main/java/com/nicksuch/weathersnap/WebActivity.java
@@ -1,20 +1,25 @@
 package com.nicksuch.weathersnap;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class WebActivity extends ActionBarActivity {
+
+    public static String defaultURL = "http://www.wunderground.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
-        WebView myWebView = (WebView) findViewById(R.id.webView);
-        myWebView.loadUrl("http://www.wunderground.com");
+        loadWeb(defaultURL, this.findViewById(R.id.webView));
     }
 
 
@@ -31,9 +36,26 @@ public class WebActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_reload) {
+            EditText editText = (EditText) findViewById(R.id.editTextUri);
+            String currentURL = editText.getText().toString();
+            if (currentURL.length() > 0 ) {
+                loadWeb(currentURL, this.findViewById(R.id.webView));
+            } else {
+                Context context = getApplicationContext();
+                CharSequence text = "Please enter a URL";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void loadWeb (String url, View view) {
+        WebView myWebView = (WebView) view;
+        myWebView.loadUrl(url);
     }
 }
