d9fa7ca35011878951932bff6d58ada3398bee37
Nick Such <nicksuch@gmail.com>
2014-10-05 21:09:07 -0400

    Make PhotoActivity fragments display in normal window (not fullscreen, not without title)

--- app/src/main/java/com/nicksuch/weathersnap/PhotoActivity.java
+++ app/src/main/java/com/nicksuch/weathersnap/PhotoActivity.java
@@ -4,8 +4,6 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Window;
-import android.view.WindowManager;
 
 
 public class PhotoActivity extends ActionBarActivity {
@@ -15,12 +13,9 @@ public class PhotoActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         report = new Report();
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
 
-        // Begin with main data entry view,
-        // PhotoFragment
+        // Begin with main data entry view,NewReportFragment
         setContentView(R.layout.activity_photo);
         FragmentManager manager = getFragmentManager();
         Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
