c932c179c8b2eaa02af2507d685e8a62bae75090
Nick Such <nicksuch@gmail.com>
2014-10-05 23:31:16 -0400

    Remove unused menu items and inflators. Ready to write tutorial.

--- app/src/main/java/com/nicksuch/weathersnap/APIActivity.java
+++ app/src/main/java/com/nicksuch/weathersnap/APIActivity.java
@@ -5,8 +5,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -72,26 +70,6 @@ public class APIActivity extends ActionBarActivity implements View.OnClickListen
         String city = editText.getText().toString();
         Log.v(LOG_TAG, "City: " + city);
         weatherTask.execute(city);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.api, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_alternate) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     // Stock Code to Fetch Weather
